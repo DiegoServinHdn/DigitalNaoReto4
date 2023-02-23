d2455b5b6f698e1e74fd7f4773a18f6d67c5f270
Diego Servin <diego.servin@xaldigital.com>
2023-02-23 16:07:17 -0600

    Complete!

--- miniproyecto_4/cuenta_regresiva.py
+++ miniproyecto_4/cuenta_regresiva.py
@@ -1,12 +1,19 @@
+import time
 from datetime import datetime
 
 NEW_YEAR = datetime.now().replace(
-    year=datetime.now().year + 1, month=1, day=1, hour=0, minute=0, second=1, microsecond=0
+    year=datetime.now().year + 1,
+    month=1,
+    day=1,
+    hour=0,
+    minute=0,
+    second=1,
+    microsecond=0,
 )
 
 
 def is_new_year(current_datetime: datetime) -> bool:
-    return NEW_YEAR == current_datetime
+    return NEW_YEAR < current_datetime
 
 
 if __name__ == "__main__":
@@ -17,4 +24,16 @@ if __name__ == "__main__":
             print("Happy New Year !!!!!!!")
             print("*" * 100)
             break
-        print(NEW_YEAR)
+        time.sleep(1)
+
+        delta = NEW_YEAR - current_datetime
+
+        days_left = delta.days
+        seconds_left = delta.seconds + (days_left * 24 * 60 * 60)
+        minutes_left = seconds_left // 60
+        hours_left = minutes_left // 60
+
+        print(
+            f"Days:{days_left} Hours:{hours_left} Minutes:{minutes_left} Seconds: {seconds_left}"
+        )
+        print("Left to New Year!! \n")
